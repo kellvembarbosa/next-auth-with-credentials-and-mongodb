436f3ae5fd4bc7a099395fce932a83ec9062378f
kellvembarbosa <kellvembarbosa@icloud.com>
2021-02-28 15:17:20 +0000

    added preloader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { Provider } from "next-auth/client";
 import React from "react";
+import NProgress from 'next-nprogress-emotion';
 
 const colors = {
     primary: {
@@ -19,9 +20,16 @@ const colors = {
 const customTheme = extendTheme({ colors });
 
 export default function App({ Component, pageProps }) {
+
     return (
         <Provider session={pageProps.session}>
             <ChakraProvider theme={customTheme}>
+                <NProgress
+                    color="#29d"
+                    options={{ trickleSpeed: 50 }}
+                    showAfterMs={300}
+                    spinner
+                />
                 <Component {...pageProps} />
             </ChakraProvider>
         </Provider>
