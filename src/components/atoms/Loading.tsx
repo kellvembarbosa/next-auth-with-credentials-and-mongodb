3fc9f9241ed8023530eb3f32a81f30bd8b3451d1
kellvembarbosa <kellvembarbosa@icloud.com>
2021-02-28 14:03:34 +0000

    fix

--- src/components/atoms/Loading.tsx
+++ src/components/atoms/Loading.tsx
@@ -1,11 +1,11 @@
 import { Box, VStack, Stack, Skeleton } from '@chakra-ui/react'
 import React from 'react'
 
-function Loading({ props }) {
+function Loading(props) {
 
 
     return (
-        <Stack>
+        <Stack {...props}>
             <Skeleton height="20px" />
             <Skeleton height="20px" />
             <Skeleton height="20px" />
